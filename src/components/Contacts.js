c9437d482564c768bfd98b0917384883a1819618
OlgaMamontova <mamontolly@gmail.com>
2022-10-20 16:58:48 +0300

    add startContacts

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -2,39 +2,39 @@ import { useState } from "react";
 import Contact from "./Contact";
 
 function getContacts (){
+
+    const startContacts =  [{
+        firstName: "Барней",
+        lastName: "Стинсовський",
+        phone: "+380956319521",
+        gender: "male"
+    }, {
+        firstName: "Робін",
+        lastName: "Щербатська",
+        phone: "+380931460123",
+        gender: "female"
+    }, {
+        firstName: "Анонімний",
+        lastName: "Анонімус",
+        phone: "+380666666666"
+    }, {
+        firstName: "Лілія",
+        lastName: "Олдровна",
+        phone: "+380504691254",
+        gender: "female"
+    }, {
+        firstName: "Маршен",
+        lastName: "Еріксонян",
+        phone: "+380739432123",
+        gender: "male"
+    }, {
+        firstName: "Теодор",
+        lastName: "Мотсбес",
+        phone: "+380956319521",
+        gender: "male"
+    }];
     
-    const [contacts, setContacts] = useState(
-         [{
-            firstName: "Барней",
-            lastName: "Стинсовський",
-            phone: "+380956319521",
-            gender: "male"
-        }, {
-            firstName: "Робін",
-            lastName: "Щербатська",
-            phone: "+380931460123",
-            gender: "female"
-        }, {
-            firstName: "Анонімний",
-            lastName: "Анонімус",
-            phone: "+380666666666"
-        }, {
-            firstName: "Лілія",
-            lastName: "Олдровна",
-            phone: "+380504691254",
-            gender: "female"
-        }, {
-            firstName: "Маршен",
-            lastName: "Еріксонян",
-            phone: "+380739432123",
-            gender: "male"
-        }, {
-            firstName: "Теодор",
-            lastName: "Мотсбес",
-            phone: "+380956319521",
-            gender: "male"
-        }],    
-    );
+    const [contacts, setContacts] = useState(startContacts);
 
     const render = (contacts) => {
         return (
@@ -45,10 +45,15 @@ function getContacts (){
     };
      
     const handleSearchChange = (value) => {
-        const filtredContacts = contacts.filter((contact) => contact.firstName.toLowerCase().includes(value.toLowerCase()) || contact.lastName.toLowerCase().includes(value.toLowerCase()) || contact.phone.includes(value))
+        if(!value){
+            setContacts(startContacts);
+            
+        } else {
+             const filtredContacts = contacts.filter((contact) => contact.firstName.toLowerCase().includes(value.toLowerCase()) || contact.lastName.toLowerCase().includes(value.toLowerCase()) || contact.phone.includes(value))
         setContacts(() => {
             return filtredContacts;
         });      
+        };      
     };
 
     return (
